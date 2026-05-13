672e065c1aeaea9eb07abc07979136c31f996c81
Kaveen Nethika <kaveenofficials@gmail.com>
2026-05-13 13:02:14 +1000

    updaet

--- packages/target_follower1/src/target_follower.py
+++ packages/target_follower1/src/target_follower.py
@@ -1,68 +1,257 @@
 #!/usr/bin/env python3
 
+
 import rospy
+
 from duckietown_msgs.msg import Twist2DStamped
-from duckietown_msgs.msg import FSMState
+
 from duckietown_msgs.msg import AprilTagDetectionArray
 
+
+
 class Target_Follower:
+
+
     def __init__(self):
-        
-        #Initialize ROS node
+
+
+        # Initialize ROS node
+
         rospy.init_node('target_follower_node', anonymous=True)
 
-        # When shutdown signal is received, we run clean_shutdown function
+
+        # Shutdown handler
+
         rospy.on_shutdown(self.clean_shutdown)
-        
-        ###### Init Pub/Subs. REMEMBER TO REPLACE "akandb" WITH YOUR ROBOT'S NAME #####
-        self.cmd_vel_pub = rospy.Publisher('/mybota002413an/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=1)
-        rospy.Subscriber('/mybota002413/apriltag_detector_node/detections', AprilTagDetectionArray, self.tag_callback, queue_size=1)
-        ################################################################
 
-        rospy.spin() # Spin forever but listen to message callbacks
 
-	    # Apriltag Detection Callback	
+        # CHANGE THIS TO YOUR ROBOT NAME
+
+        self.robot_name = "mybota002413an"
+
+
+        # Publisher
+
+        self.cmd_vel_pub = rospy.Publisher(
+
+            f'/{self.robot_name}/car_cmd_switch_node/cmd',
+
+            Twist2DStamped,
+
+            queue_size=1
+
+        )
+
+
+        # Subscriber
+
+        rospy.Subscriber(
+
+            f'/{self.robot_name}/apriltag_detector_node/detections',
+
+            AprilTagDetectionArray,
+
+            self.tag_callback,
+
+            queue_size=1
+
+        )
+
+
+        rospy.loginfo("Target follower node started.")
+
+
+        rospy.spin()
+
+
+
+    # Callback function
+
     def tag_callback(self, msg):
+
+
         self.move_robot(msg.detections)
- 
-    # Stop Robot before node has shut down. This ensures the robot keep moving with the latest velocity command
+
+
+
+    # Clean shutdown
+
     def clean_shutdown(self):
-        rospy.loginfo("System shutting down. Stopping robot...")
+
+
+        rospy.loginfo("Shutting down. Stopping robot...")
+
         self.stop_robot()
 
-    # Sends zero velocity to stop the robot
+
+
+    # Stop robot completely
+
     def stop_robot(self):
+
+
         cmd_msg = Twist2DStamped()
+
         cmd_msg.header.stamp = rospy.Time.now()
+
+
         cmd_msg.v = 0.0
+
         cmd_msg.omega = 0.0
+
+
         self.cmd_vel_pub.publish(cmd_msg)
+
+
+
+    # Main movement function
 
     def move_robot(self, detections):
 
-        #### YOUR CODE GOES HERE ####
+
+        cmd_msg = Twist2DStamped()
+
+        cmd_msg.header.stamp = rospy.Time.now()
+
+
+        ###################################################
+
+        # TASK 1 - SEEK OBJECT FEATURE
+
+        ###################################################
+
+
+        # If no object/tag detected
 
         if len(detections) == 0:
+
+
+            rospy.loginfo("No object detected. Seeking object...")
+
+
+            # Rotate slowly to search
+
+            cmd_msg.v = 0.0
+
+            cmd_msg.omega = 1.0
+
+
+            self.cmd_vel_pub.publish(cmd_msg)
+
+
             return
 
+
+
+        ###################################################
+
+        # TASK 2 - LOOK AT OBJECT FEATURE
+
+        ###################################################
+
+
+        # Get tag position
+
         x = detections[0].transform.translation.x
+
         y = detections[0].transform.translation.y
+
         z = detections[0].transform.translation.z
 
-        rospy.loginfo("x,y,z: %f %f %f", x, y, z)
+
+        rospy.loginfo("Object detected")
+
+        rospy.loginfo("x: %f", x)
+
+        rospy.loginfo("y: %f", y)
+
+        rospy.loginfo("z: %f", z)
 
 
-        # Publish a velocity
-        cmd_msg = Twist2DStamped()
-        cmd_msg.header.stamp = rospy.Time.now()
+        ###################################################
+
+        # Proportional Controller
+
+        ###################################################
+
+
+        # Error is horizontal displacement
+
+        error = x
+
+
+        # Proportional gain
+
+        k_p = 3.0
+
+
+        # Compute angular velocity
+
+        omega = -k_p * error
+
+
+        ###################################################
+
+        # Velocity Limits
+
+        ###################################################
+
+
+        # Maximum turning speed
+
+        if omega > 3.0:
+
+            omega = 3.0
+
+
+        elif omega < -3.0:
+
+            omega = -3.0
+
+
+        ###################################################
+
+        # Dead Zone
+
+        ###################################################
+
+
+        # Prevent shaking when object is centered
+
+        if abs(error) < 0.03:
+
+            omega = 0.0
+
+
+        ###################################################
+
+        # Publish Velocity
+
+        ###################################################
+
+
+        # No forward/backward motion
+
         cmd_msg.v = 0.0
-        cmd_msg.omega = 0.0
+
+
+        # Only rotation
+
+        cmd_msg.omega = omega
+
+
         self.cmd_vel_pub.publish(cmd_msg)
 
-        #############################
+
 
 if __name__ == '__main__':
+
+
     try:
+
         target_follower = Target_Follower()
+
+
     except rospy.ROSInterruptException:
+
         pass
